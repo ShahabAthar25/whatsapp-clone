545916fa3c92020fe4483827904bba57a9f96eb4
ShahabAthar25 <shahabathar25@gmail.com>
2021-07-11 19:42:04 +0500

    Add Component

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDe4QJawS-Y2igQEsp39UdRaWvb2aVloFo",
+  authDomain: "whatsapp-mern-aa622.firebaseapp.com",
+   projectId: "whatsapp-mern-aa622",
+  storageBucket: "whatsapp-mern-aa622.appspot.com",
+  messagingSenderId: "1026985583435",
+  appId: "1:1026985583435:web:b8e333f99988a0b15bbece"
+};
